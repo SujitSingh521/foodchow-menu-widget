9346c1e320b5b0419794ebdfe485c1a4ab1b9462
Sujit Kumar <5215ssingh@gmail.com>
2025-09-15 10:02:52 +0530

    updated

--- foodchow-frontend/src/components/Sidebar.jsx
+++ foodchow-frontend/src/components/Sidebar.jsx
@@ -1,7 +1,17 @@
 export function Sidebar({ categories, activeCategory, setActiveCategory }) {
   return (
-    <aside className="w-[28%] ml-9 p-4 bg-white rounded-lg shadow-md border border-gray-200 sticky top-24 h-[68vh] overflow-auto">
-      <h2 className="font-bold text-lg mb-4 text-gray-900">CATEGORIES</h2>
+    <aside className="w-full lg:w-[28%] ml-0 lg:ml-9 p-4 bg-white rounded-lg shadow-md border border-gray-200 sticky top-24 h-[70vh] overflow-auto">
+      <h2
+        className="font-bold text-lg mb-4 pb-2"
+        style={{
+          color: "var(--dynamic-color)",
+          borderBottom: "1px dashed rgb(204, 204, 204)",
+        }}
+      >
+        CATEGORIES
+      </h2>
+
+      {/* Category list */}
       <ul className="space-y-1">
         {categories.map((cat, i) => (
           <li
